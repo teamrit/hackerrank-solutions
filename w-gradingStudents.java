2c8e6ea8183d349b49c1997756c401338e017a9e
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-02-18 04:35:10 -0500

    Create w-gradingStudents.java

---
+++ w-gradingStudents.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int[] grades){
+        // Complete this function
+        for (int j=0; j<grades.length; j++) {
+            if (grades[j]>=38) {
+                if (grades[j]%5>=3) {
+                    grades[j] = grades[j] + 5-(grades[j]%5);
+                }
+            }
+        }
+        return grades;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] grades = new int[n];
+        for(int grades_i=0; grades_i < n; grades_i++){
+            grades[grades_i] = in.nextInt();
+        }
+        int[] result = solve(grades);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
