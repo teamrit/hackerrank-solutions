4f3ef84918ae6a6b5af221a80fcf093b9ae89eb9
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-02-18 04:34:39 -0500

    Create w-birthdayCakeCandles.java

---
+++ w-birthdayCakeCandles.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        // Complete this function
+        Arrays.sort(ar);
+        int a = ar[ar.length-1];
+        int occ=0;
+        for (int i=0; i<ar.length; i++) {
+            if (ar[i]==a){
+                occ++;
+            }
+        }
+        return occ;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+}
